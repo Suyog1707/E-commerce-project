2765e5abd2a1f5cd9b847d9f4c840fb36f942f59
Suyog1707 <148788280+Suyog1707@users.noreply.github.com>
2026-01-25 17:00:43 +0530

    Update links to match with router and Add orders and tracking pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import HomePage from './pages/HomePage'
+import CheckoutPage from './pages/CheckoutPage'
+import OrdersPage from './pages/OrdersPage'
+import { Routes, Route } from 'react-router'
 import './App.css'
+import TrackingPage from './pages/TrackingPage'
 
 function App() {
 
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
+      </Routes>
     </>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,12 @@
 import './HomePage.css';
-import './HomePage-header.css';
+import './header.css';
 
 function HomePage() {
     return (
         <>
             <div class="header">
             <div class="left-section">
-                <a href="index.html" class="header-link">
+                <a href="/" class="header-link">
                 <img class="logo"
                     src="images/logo-white.png" />
                 <img class="mobile-logo"
@@ -23,12 +23,12 @@ function HomePage() {
             </div>
 
             <div class="right-section">
-                <a class="orders-link header-link" href="orders.html">
+                <a class="orders-link header-link" href="/orders">
 
                 <span class="orders-text">Orders</span>
                 </a>
 
-                <a class="cart-link header-link" href="checkout.html">
+                <a class="cart-link header-link" href="/checkout">
                 <img class="cart-icon" src="images/icons/cart-icon.png" />
                 <div class="cart-quantity">3</div>
                 <div class="cart-text">Cart</div>
